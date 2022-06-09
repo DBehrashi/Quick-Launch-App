6a85679605cfdc43ff8dafd52165194499095489
DBehrashi <dari.behrashi@gmail.com>
2022-06-09 15:54:08 -0700

    Finished MVP

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 9000
 server.use(express.json())
 server.use(cors())
 
-server.get('api/hello', (req, res) => {
+server.get('/api/hello', (req, res) => {
     res.json({message: 'API is working'})
 })
 
